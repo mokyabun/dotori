4cefe1ed452b25fdefcf333842c81b07cc42dc05
mokyabun <mokyabun@gmail.com>
2026-06-21 00:15:13 +0900

    Add macmini app startup config

--- config/index.ts
+++ config/index.ts
@@ -1,12 +1,16 @@
+import os from 'node:os'
 import { type Context, defineConfig } from 'dotori'
 import desktop from './desktop'
 import developer from './developer'
+import macmini from './macmini'
 import qol from './qol'
 import security from './security'
 import settings from './settings'
 import sync from './sync'
 
 export default defineConfig((ctx: Context) => {
+    const hostname = process.env.HOSTNAME || os.hostname()
+
     ctx.brew.install('mas')
 
     developer(ctx)
@@ -14,6 +18,7 @@ export default defineConfig((ctx: Context) => {
     ctx.group('qol', (g) => qol(g))
     ctx.group('sync', (g) => sync(g))
     ctx.group('desktop', (g) => desktop(g))
+    if (hostname === 'macmini') ctx.group('macmini', (g) => macmini(g))
 
     ctx.group('settings', (g) => settings(g), {
         hooks: {
--- config/qol.ts
+++ config/qol.ts
@@ -4,8 +4,12 @@ export default (ctx: Context) => {
     ctx.brew.cask('discord')
     ctx.brew.cask('parsec')
     ctx.brew.cask('brave-browser')
-    ctx.brew.cask('linearmouse')
     ctx.brew.cask('jordanbaird-ice')
+    ctx.launchd.agent('ice', {
+        ProgramArguments: ['/Applications/Ice.app/Contents/MacOS/Ice'],
+        RunAtLoad: true,
+        KeepAlive: true,
+    })
     ctx.brew.cask('bambu-studio')
     ctx.brew.cask('blender')
 }
